267c98efa32110a0f03f067c03448b729fde3325
Prakhar21Agnihotri <74812498+Prakhar21Agnihotri@users.noreply.github.com>
2023-08-03 00:28:09 +0530

    Update Stocks.py

--- Stocks.py
+++ Stocks.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import pandas_datareader.data as web
+import yfinance as yf
+from yahoofinancials import YahooFinancials
 from Func import *
 from Lstm_Forecast import*
 
@@ -14,8 +16,13 @@ Selected = dict()
 Selected_LSTM = dict()
 Selected_Movig_Avg, Selected_Stock, start, end = Page("Stocks","Stocks",Stocks)
 if Selected_Stock not in Selected.keys():
-    Selected[Selected_Stock] = web.DataReader(Stocks.loc[Selected_Stock][0], 
-    data_source = 'yahoo',start= start , end = end)
+    Selected = dict()
+Selected_LSTM = dict()
+Selected_Movig_Avg, Selected_Stock, start, end = Page("Stocks","Stocks",Stocks)
+if Selected_Stock not in Selected.keys():
+    Selected[Selected_Stock] = yf.download(Stocks.loc[Selected_Stock][0], start= start , end = end,  progress=False)
+    #Selected[Selected_Stock] = web.DataReader(Stocks.loc[Selected_Stock][0], 
+    #data_source = 'yahoo',start= start , end = end)
 
     
 
