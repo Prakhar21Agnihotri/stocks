1169eefe7e25737c2983ead5ed53011ebdc9808d
Prakhar21Agnihotri <74812498+Prakhar21Agnihotri@users.noreply.github.com>
2023-08-03 01:24:56 +0530

    Stocks.py

--- Stocks.py
+++ Stocks.py
@@ -2,7 +2,7 @@ import pandas as pd
 import yfinance as yf
 yf.pdr_override() # <== that's all it takes :-)
 from pandas_datareader import data as pdr
-#import pandas_datareader.data as web
+import pandas_datareader.data as web
 import yfinance as yf
 
 from Func import *
@@ -23,9 +23,9 @@ if Selected_Stock not in Selected.keys():
 Selected_LSTM = dict()
 Selected_Movig_Avg, Selected_Stock, start, end = Page("Stocks","Stocks",Stocks)
 if Selected_Stock not in Selected.keys():
-    Selected[Selected_Stock] = pdr.get_data_yahoo(Stocks.loc[Selected_Stock][0], start=start, end=end)
-    #Selected[Selected_Stock] = web.DataReader(Stocks.loc[Selected_Stock][0], 
-    #data_source = 'yahoo',start= start , end = end)
+    #Selected[Selected_Stock] = pdr.get_data_yahoo(Stocks.loc[Selected_Stock][0], start=start, end=end)
+    Selected[Selected_Stock] = web.DataReader(Stocks.loc[Selected_Stock][0], 
+    data_source = 'stooq',start= start , end = end)
 
     
 
